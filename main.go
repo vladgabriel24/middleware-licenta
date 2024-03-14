c1d9d335db0652a04a5c92b361b99186f672a1ef
vladgabriel24 <vlad.dumitru2402@gmail.com>
2024-03-14 20:37:32 +0200

    Implement the credential config file and the usage in the codebase

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/ini.v1"
+)
+
 func main() {
 
-	// De citit dintr-un fisiere de configurare .ini
-	// Database env variables
-	userdb := "admin"
-	passdb := "licenta"
-	ipdb := "localhost"
-	portdb := 3306
-	rootpass := "licenta"
+	inidata, err := ini.Load("config.ini")
+	if err != nil {
+		fmt.Printf("Fail to read the config file: %v", err)
+		os.Exit(1)
+	}
 
-	// Middleware env variables
-	ip := "192.168.0.102"
-	port := 8080
+	env := inidata.Section("environment")
+	sistem := inidata.Section("sistem")
+	database := inidata.Section("database")
+
+	userdb := database.Key("user").String()
+	passdb := database.Key("password").String()
+	ipdb := database.Key("ip").String()
+	portdb, _ := database.Key("port").Int()
+
+	rootpass := sistem.Key("rootpassword").String()
+
+	ip := env.Key("ip").String()
+	port, _ := env.Key("port").Int()
 
 	db := initDB(userdb, passdb, ipdb, portdb)
 	initRouters(ip, port, rootpass, db)
