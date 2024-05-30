f4eebfe808fd319389b4b4d23aca256e4daa0654
vladgabriel24 <vlad.dumitru2402@gmail.com>
2024-05-31 01:10:05 +0300

    Integration with frontend part 1

--- routers.go
+++ routers.go
@@ -10,26 +10,26 @@ import (
 func initRouters(ip string, port int, rootpass string, database *sql.DB) {
 
 	router := gin.Default()
-	router.GET("/get-nume", getNume)
-	router.GET("/get-serial", func(ctx *gin.Context) {
+	router.GET("/api/get-nume", getNume)
+	router.GET("/api/get-serial", func(ctx *gin.Context) {
 		getSerial(ctx, rootpass)
 	})
-	router.GET("/get-furnizor", getFurnizor)
-	router.GET("/get-procesor", getProcesor)
-	router.GET("/get-placi-retea", getPlaciRetea)
-	router.GET("/get-stare-placa-retea", getStarePlacaRetea)
-	router.GET("/get-date-transmise-placa-retea", getDateTransmisePlacaRetea)
-	router.GET("/get-date-receptionate-placa-retea", getDateReceptionatePlacaRetea)
-	router.GET("/get-date-aruncate-placa-retea", getDateAruncatePlacaRetea)
-	router.GET("/get-utilizare-disk", func(ctx *gin.Context) {
+	router.GET("/api/get-furnizor", getFurnizor)
+	router.GET("/api/get-procesor", getProcesor)
+	router.GET("/api/get-placi-retea", getPlaciRetea)
+	router.GET("/api/get-stare-placa-retea", getStarePlacaRetea)
+	router.GET("/api/get-date-transmise-placa-retea", getDateTransmisePlacaRetea)
+	router.GET("/api/get-date-receptionate-placa-retea", getDateReceptionatePlacaRetea)
+	router.GET("/api/get-date-aruncate-placa-retea", getDateAruncatePlacaRetea)
+	router.GET("/api/get-utilizare-disk", func(ctx *gin.Context) {
 		getUtilizareDisk(ctx, rootpass)
 	})
-	router.GET("/get-utilizare-RAM", getUtilizareRAM)
-	router.GET("/get-utilizare-CPU", getUtilizareCPU)
-	router.POST("/load-db", func(ctx *gin.Context) {
+	router.GET("/api/get-utilizare-RAM", getUtilizareRAM)
+	router.GET("/api/get-utilizare-CPU", getUtilizareCPU)
+	router.POST("/api/load-db", func(ctx *gin.Context) {
 		loadDB(ctx, database, rootpass)
 	})
-	router.POST("/trigger-cron_db", func(ctx *gin.Context) {
+	router.POST("/api/trigger-cron_db", func(ctx *gin.Context) {
 		triggerLoadDB(ctx, rootpass, ip)
 	})
 
